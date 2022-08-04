1d6abf7ce574986ff0d90bf276a4cc8e4b057d6f
laona <laonane@qq.com>
2022-08-04 20:40:48 +0800

    refact(优化代码)

--- small-spring-step-01/src/main/java/wiki/laona/framework/BeanDefinition.java
+++ small-spring-step-01/src/main/java/wiki/laona/framework/BeanDefinition.java
@@ -7,18 +7,11 @@ public class BeanDefinition {
 
     private Object bean;
 
-    public BeanDefinition() {
-    }
-
     public BeanDefinition(Object bean) {
         this.bean = bean;
     }
 
     public Object getBean() {
         return bean;
-    }
-
-    public void setBean(Object bean) {
-        this.bean = bean;
     }
 }
