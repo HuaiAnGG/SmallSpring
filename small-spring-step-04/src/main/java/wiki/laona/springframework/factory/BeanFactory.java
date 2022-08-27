76eaea736a48a876dda2974d286739db4c28fdac
laona <laonane@qq.com>
2022-08-27 13:19:20 +0800

    feat(第 4 章：崭露头角，基于Cglib实现含构造函数的类实例化策略)

---
+++ small-spring-step-04/src/main/java/wiki/laona/springframework/factory/BeanFactory.java
@@ -0,0 +1,15 @@
+package wiki.laona.springframework.factory;
+
+import wiki.laona.springframework.BeansException;
+
+/**
+ * @author laona
+ * @description
+ * @date 2022-08-04 15:22
+ **/
+public interface BeanFactory {
+
+    public Object getBean(String name) throws BeansException;
+
+    public Object getBean(String name, Object[] args) throws BeansException;
+}
---
+++ small-spring-step-04/src/main/java/wiki/laona/springframework/factory/support/InstantiationStrategy.java
@@ -0,0 +1,17 @@
+package wiki.laona.springframework.factory.support;
+
+import wiki.laona.springframework.BeansException;
+import wiki.laona.springframework.factory.config.BeanDefinition;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Created by laona
+ **/
+public interface InstantiationStrategy {
+
+    Object instantiate(BeanDefinition beanDefinition,
+                       String beanName,
+                       Constructor ctor,
+                       Object[] args) throws BeansException;
+}
